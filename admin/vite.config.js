62f97d9857bd15ca78cedd652beaf31ffdf3ace6
Manoj-Krishna-Chandragiri <manojkrishnachandragiri@gmail.com>
2025-04-24 18:31:17 +0530

    Update Vite configuration for frontend and admin build output directories

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'admin/dist', // Set the build output directory to admin/dist
+    outDir: 'dist', // Set the build output directory to admin/dist
   },
 })
